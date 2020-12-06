5947f240b3f10a2253be597faa6c931c5429ebc5
Berhanu2011 <73730169+Berhanu2011@users.noreply.github.com>
2020-12-05 19:15:11 -0500

    Last update

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+
+// Home page
+import Navigation from "./Components/Nav/Nav";
+import Footer from "./Components/Footer/Footer";
+import Main from "./Components/Main/Main";
+
+// Pages
+import Iphone from "./Pages/Iphone/iphone";
+import Mac from "./Pages/Mac/Mac";
+import Four04 from "./Pages/Four04/Four04";
+import Productpage from "./Pages/Productpage/Productpage";
+
+// import general css
+import "./css/styles.css";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navigation />
+        <Switch>
+          <Route path="/" exact component={Main} />
+          <Route path="/mac" exact component={Mac} />
+          <Route path="/iphone" exact component={Iphone} />
+          <Route path="/iphone/:pid" exact component={Productpage} />
+          <Route path="/" component={Four04} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
